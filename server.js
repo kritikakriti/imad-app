94896fc8956147eec3d9b008150098455b8a5052
kritikakriti <33561960+kritikakriti@users.noreply.github.com>
2018-02-18 17:47:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -96,6 +96,10 @@ app.get('/:articleName',function(req, res){
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.css'));
+});
+
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
