eb90d3c6d6993efa307f2c4b72c9692e2badae3d
kritikakriti <33561960+kritikakriti@users.noreply.github.com>
2018-02-19 13:54:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,10 +9,10 @@ element.innerHTML = 'Hasura-web-app';
 var img = document.getElementById('madi');
 var marginLeft = 0;
 function moveRight () {
-    marginLeft = marginLeft+10;
+    marginLeft = marginLeft + 5;
     img.style.marginLeft = marginLeft + 'px';
 }
  img.onClick = function () {
-     var interval = setInterval(moveRight, 100);
+     var interval = setInterval(moveRight, 50);
 };
 
