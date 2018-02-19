2e55ca4678b0b9877e838fd35c615effab147631
kritikakriti <33561960+kritikakriti@users.noreply.github.com>
2018-02-19 13:16:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,11 +2,12 @@ console.log('Loaded!');
 
 //Change the text of the main-text div
 var element=document.getElementById('main-text');
-element.innerHTML='Hasura-web-app';
+element.innerHTML = 'Hasura-web-app';
+
 
 //move the image
 var img = document.getElementById('madi');
- img.onClick = function(){
+ img.onClick = function () {
     img.style.marginLeft = '100px';
 };
 
