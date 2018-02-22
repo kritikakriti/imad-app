91eac55d0d1db47171df567cf618c94f2ecd657e
kritikakriti <33561960+kritikakriti@users.noreply.github.com>
2018-02-22 10:27:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -113,9 +113,9 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 var names = [];
-app.get('/submit-name/:name', function(req, res){
+app.get('/submit-name', function(req, res){//URL:submit-name?name, name=xxxxx
     //get the name from the request
-    var name = req.params.name;//TODO
+    var name = req.query.name;//TODO
     names.push(name);
     //JSON:Java Script Object Notation
     res.send(JSON.stringify(names)); //TODO
