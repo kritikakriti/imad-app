7b9e7e72025bcc72fce0d297d8961e60d81c515c
kritikakriti <33561960+kritikakriti@users.noreply.github.com>
2018-02-23 08:03:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,7 +24,7 @@ button.onclick = function () {
         //Not done yet
     };
     //Make a request
-    request.open('GET', 'http://kritikakriti145.imad.hasura-app.io/submit-name?name =' + name, true);
+    request.open('GET', 'http://kritikakriti145.imad.hasura-app.io/counter', true);
     request.send(null);
 };
 
@@ -33,11 +33,33 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
-    //Make a request to the server and send the names
+
+     //Create a request object
+    var request = new XMLHttpRequest();
     
-   
-    };
-    var ul = document.getElementById('namelist');
+    
+    //capture a response and store it in a variable
+    request.onreadystatechange = function () {
+        if(request.readyState === XMLHttpRequest.DONE){
+            //Take some action
+            if (request.status === 200) {
+                 //capture the names and render it as a list
+    var names = request.responseText;
+    names = JSON.parse(names);
+    var list = '';
+    for (var i=0; i<names.length; i++){
+        list += '<li>' + names[i] + '<li>';
+    }
+   var ul = document.getElementById('namelist');
     ul.innerHTML = list;
-    
+ }
+        }
+};
+//Submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+//Make a request
+    request.open('GET', 'http://kritikakriti145.imad.hasura-app.io/submit-name?name =' + name, true);
+    request.send(null);
+
 };
