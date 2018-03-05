78c98ea1c7292c173b7239d1ccc865ce44e16aa9
kritikakriti <33561960+kritikakriti@users.noreply.github.com>
2018-03-05 08:07:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool = require('pg').pool;
+
+var config = {
+    user: 'kritikakriti145',
+    database:'kritikakriti145',
+    host: 'db.imad.hasura.io',
+    port:'5432',
+    password:process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -87,9 +96,17 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var pool = new Pool(config);
 app.get('/test-db', function (req, res) {
   //Make a select request
   //return a response with the result
+  pool.query('SELECT * FROM test', function (err, result){
+      if(err){
+          res.status(500).send(err.toString());
+      }else{
+          res.send(JSON.stringify(result));
+      }
+  })
 });
 
 var counter = 0;
